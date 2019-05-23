8c3d45e2fed61c2e1d80efbf64978c7f1c4dd8bf
Ilham Kusuma Aji <ilhamkusumaaji@yahoo.com>
2019-05-23 14:00:10 +0700

    update data

--- sarpras.sql
+++ sarpras.sql
@@ -53,12 +53,44 @@ CREATE TABLE transaksi (
 INSERT INTO acara values (01,'Forsi','Gor Jati');
 INSERT INTO acara values (02,'Unpad Awards','Graha Sanusi');
 INSERT INTO acara values (03,'Padjadjaran Education Festival','Lapangan Parkir PPBS');
+INSERT INTO acara values (04,'Informatics Festival','Graha Sanusi');
+INSERT INTO acara values (05,'Technopreneur','Gedung 2 Hukum');
+INSERT INTO acara values (06,'Minang Debat Art Festival','Graha Sanusi');
+INSERT INTO acara values (07,'Reuni Alumni UPBM','Bale Basket');
+INSERT INTO acara values (08,'Statistics Day','Lapangan Parkir PPBS');
+INSERT INTO acara values (09,'Parafest','Lapangan Eks Kopma');
+INSERT INTO acara values (10,'Market Town','Lapangan Eks Kopma');
+INSERT INTO acara values (11,'Prabu','Gor Jati');
+INSERT INTO acara values (12,'Olahraga Bersama Himatif','Gor Jati');
+INSERT INTO acara values (13,'TEDXUnpad','Bale Santika');
+INSERT INTO acara values (14,'MIPA Award','Bale Santika');
+INSERT INTO acara values (15,'Seminar Aku Project','Bale Rumawa');
+INSERT INTO acara values (16,'Forsi','Gor Jati');
+INSERT INTO acara values (17,'Seminar Kewirausahaan BEM MIPA','Aula PPBS');
+INSERT INTO acara values (18,'Kumpul Alumni Informatika','Aula PPBS');
+INSERT INTO acara values (19,'Intention','Arboretum');
+INSERT INTO acara values (20,'Seminar Kesekretariatan','UDJT0201');
 
-
-INSERT INTO peminjam values (140810160019,'Herizal Kurniawan','L');
-INSERT INTO peminjam values (140810160021,'Ilham Kusuma Aji','L');
-INSERT INTO peminjam values (140810160019,'Herizal Kurniawan','L');
-INSERT INTO peminjam values (140810160019,'Herizal Kurniawan','L');
+INSERT INTO peminjam values (140810160019,'HERIZAL KURNIAWAN','L');
+INSERT INTO peminjam values (140810160021,'ILHAM KUSUMA AJI','L');
+INSERT INTO peminjam values (210510160082,'KINANTI SOPHIA RAMADHAN','P');
+INSERT INTO peminjam values (210210160007,'SITI HAWA AMALIA','P');
+INSERT INTO peminjam values (170104160042,'JERICHO MANUEL','L');
+INSERT INTO peminjam values (180410160048,'ERES FERRO BASTIAN','L');
+INSERT INTO peminjam values (140410160025,'ABIGAIL MARUANAYA','P');
+INSERT INTO peminjam values (240110160117,'KEMAL MAULANA RAHMAN SUHAENDI P','L');
+INSERT INTO peminjam values (240210160077,'BELLA AMALIA HARINDIRA','P');
+INSERT INTO peminjam values (120310160016,'YUSUF RAHMAT ABDILLAH','L');
+INSERT INTO peminjam values (120210160005,'MUHAMMAD IBNU FEBRYAN RUSLAN','L');
+INSERT INTO peminjam values (130104160020,'NENG FITRI','P');
+INSERT INTO peminjam values (170410160082,'ELVA NURFAUZIAH','P');
+INSERT INTO peminjam values (140910160023,'DIMAS ADRIAN MUKTI','L');
+INSERT INTO peminjam values (240210160045,'MUHAMMAD ARIJUDDIN','L');
+INSERT INTO peminjam values (120104160055,'SISKA HAIBAH','P');
+INSERT INTO peminjam values (170204160049,'LATHIFA METRYLIA','P');
+INSERT INTO peminjam values (180510160068,'YOGA HEDIASA','L');
+INSERT INTO peminjam values (240210160075,'NADILA ZULFA FITHRIYAH','P');
+INSERT INTO peminjam values (210410160001,'DINAR NAFRASANIA','P');
 
 
 INSERT INTO petugas values (01,'Suparman','L');
